967d0daa3e5369009267156b6a9b97c091d0f97b
Kevin Barbosa <kevinbbst@gmail.com>
2025-01-10 22:35:06 -0300

    Atualização de alguns textos

--- src/Components/About.js
+++ src/Components/About.js
@@ -12,17 +12,22 @@ export default function About() {
 
             <div>
                 <h1 class="text-3xl pb-2">
-                    Olá, sou o{" "}
+                    Olá, me chamo{" "}
                     <mark class="text-pink-600 bg-transparent">Kevin</mark>
                 </h1>
                 <p class="max-w-sm md:max-w-xl pb-2">
-                    Tenho 5 anos de experiência como designer gráfico e
-                    atualmente estou no{" "}
-                    <b>1º semestre de Engenharia de Software</b>, em busca da
-                    minha primeira oportunidade como desenvolvedor. Pretendo
-                    aplicar minhas experiências anteriores e as habilidades que
-                    adquiri nesta nova fase da minha carreira, sempre em busca
-                    de novos desafios e aprendizados!
+                    Sou designer gráfico com 5 anos de experiência e atualmente
+                    estou cursando o{" "}
+                    <b>
+                        1º semestre de Análise e Desenvolvimento de Sistemas na
+                        Mackenzie.
+                    </b>
+                    Estou em busca da minha primeira oportunidade como
+                    desenvolvedor, para aplicar as habilidades adquiridas ao
+                    longo da minha trajetória no design e nesta nova fase de
+                    aprendizado em tecnologia. Estou em busca de desafios que me
+                    permitam crescer profissionalmente e contribuir de forma
+                    significativa.
                 </p>
                 <button
                     class="flex items-center w-fit h-fit pl-4 pr-10 py-2.5 text-neutral-300 bg-neutral-800 border 
